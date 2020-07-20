560be8963f132e8e1699937859dca889c4b656fa
Demonator113 <68544210+Demonator113@users.noreply.github.com>
2020-07-20 13:02:54 +0100

    Update Sword.cs

--- Items/Sword.cs
+++ Items/Sword.cs
@@ -13,7 +13,7 @@ namespace SuperTerraMod.Items
 
 		public override void SetDefaults() 
 		{
-			item.damage = 50;
+			item.damage = 70;
 			item.melee = true;
 			item.width = 40;
 			item.height = 40;
